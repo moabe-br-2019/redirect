e9d53c4f4ae2a0a87428343f8338f9760d40f131
Moabe <54943821+moabe-br-2019@users.noreply.github.com>
2023-10-11 17:32:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+import { Response } from 'cloudflare/workers'
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 async function handleRequest(request) {
   const { pathname } = new URL(request.url)
   // Redireciona todas as solicitações para https://www.dominio-de-destino.com.br
-  return Response.redirect(`https://orcamento-riocordas.bubbleapps.io/${pathname}`, 301)
+  return Response.redirect(`https://www.dominio-de-destino.com.br${pathname}`, 301)
 }
