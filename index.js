24f95a1f655e4716135867827024623b895aff96
Moabe <54943821+moabe-br-2019@users.noreply.github.com>
2023-10-11 17:25:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
   const { pathname } = new URL(request.url)
   // Redireciona todas as solicitações para https://www.dominio-de-destino.com.br
-  return Response.redirect(`https://orcamento-riocordas.bubbleapps.io${pathname}`, 301)
+  return Response.redirect(`https://orcamento-riocordas.bubbleapps.io/${pathname}`, 301)
 }
